ad4b5fef82a172918a794e07cef61f6a8ae32f02
David Dennerline <ddennerline@gmail.com>
2020-02-20 17:40:48 -0500

    Updated documentation

--- new_command.py
+++ new_command.py
@@ -1,6 +1,8 @@
 def main():
+    """This function starts processing of new commands."""
     print("hello world")
 
 
 if __name__ == "__main__":
+    """This function starts processing of new commands."""
     main()
