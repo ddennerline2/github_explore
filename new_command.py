a20d8e17cecbf19a2668a894cff57d1d0978dfee
David Dennerline <ddennerline@gmail.com>
2020-02-20 17:43:41 -0500

    Added new command
    Updated documentation
    
    
    Added arguments

---
+++ new_command.py
@@ -0,0 +1,8 @@
+def main(args):
+    """This function starts processing of new commands."""
+    print(f"hello world: {args}")
+
+
+if __name__ == "__main__":
+    """This function starts processing of new commands."""
+    main(("--args", "/home/example"))
