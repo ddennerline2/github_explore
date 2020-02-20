0fd9a5c9c619371eca3526cfc5e62eac7b9ef905
David Dennerline <ddennerline@gmail.com>
2020-02-20 18:33:26 -0500

    Revert "Added change 1"
    This reverts commit fe490df65229ac5d53377e2c925eb44868856f13.

--- new_command.py
+++ new_command.py
@@ -4,9 +4,6 @@ def main(args):
     print("ran to store")
     print("bought fruit")
     print("bought rice")
-    print("Change1")
-    print("Change2")
-    print("Change3")
 
 
 if __name__ == "__main__":
