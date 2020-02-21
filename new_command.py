a054e0acb1e331cfbb8080d56e83a66fd3b0d651
ddennerline2 <ddennerline@gmail.com>
2020-02-20 19:05:21 -0500

    CR003 - Added apple, sugar (#3)
    * Added sugar, apple, egg

--- new_command.py
+++ new_command.py
@@ -4,6 +4,12 @@ def main(args):
     print("ran to store")
     print("bought fruit")
     print("bought rice")
+    print("bought sugar")
+    print("bought apple")
+    print("bought egg")
+    print("bought bread")
+    print("bought cherry")
+    print("bought grape")
 
 
 if __name__ == "__main__":
