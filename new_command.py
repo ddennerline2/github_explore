cf7d47ee809cb7d7ebe33b0d46bf52a0f2bc87c1
ddennerline2 <ddennerline@gmail.com>
2020-04-27 14:57:08 -0400

    CR-004: Printed food catagories (#9)
    * fix(CR-004): Added print_fruits/starches

--- new_command.py
+++ new_command.py
@@ -3,19 +3,24 @@ import datetime as dt
 def print_timestamp():
     print("Date now", dt.datetime.now())
 
+def print_fruits(fruits):
+    for fruit in fruits:
+        print(f"bought {fruit}")
+
+def print_starches(starches):
+    for starch in starches:
+        print(f"bought {starch}")
+
+
 def main(args):
     """This function starts processing of new commands."""
     print(f"hello world: {args}")
     print_timestamp()
     print("ran to store")
-    print("bought fruit")
-    print("bought rice")
+    print_fruits(("apple", "cherry", "grape"))
+    print_starches(("rice", "bread"))
     print("bought sugar")
-    print("bought apple")
     print("bought egg")
-    print("bought bread")
-    print("bought cherry")
-    print("bought grape")
 
 
 if __name__ == "__main__":
